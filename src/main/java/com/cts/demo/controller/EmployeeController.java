7ddf7576e2310de8fd0c663be3b11b733470d009
901465 <901465@LTIN321899.cts.com>
2025-03-13 10:57:59 +0530

    update

--- src/main/java/com/cts/demo/controller/EmployeeController.java
+++ src/main/java/com/cts/demo/controller/EmployeeController.java
@@ -11,13 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.cts.demo.model.Employee;
 import com.cts.demo.service.EmployeeService;
 
 @RestController
-@RequestMapping("employee")
 public class EmployeeController {
 	
 	@Autowired
